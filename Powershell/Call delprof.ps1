552e21c3427976c74099fc3c42861601d113c25f
cmeylan <159033943+Theeb7274@users.noreply.github.com>
2025-05-19 12:50:18 +0100

    Update and rename Call delprof.ps1 to Call another ps1.ps1

--- Powershell/Call delprof.ps1
+++
@@ -1,6 +0,0 @@
-﻿$script = "\\ad.sfet.org.uk\SysVol\ad.sfet.org.uk\Policies\{8EAA54E3-ABDB-4081-9659-65F22B191C12}\Machine\Scripts\Shutdown\del_prof_wrapper.ps1"
-$exclusions = "admin", "administrator", "public", "default"
-$path = "\\sfet-bhcs-mgt01\software$\Delprof2 1.6.0"
-$logfile = "C:\EDUIT\Delprof_$(Get-Date -F 'yyyyMMdd-HHmmss')log.txt"
-
-& $script -Unattended -ExcludeUsers $exclusions -Verbose -Logfile $logfile -DelprofPath $path -ListOnly
