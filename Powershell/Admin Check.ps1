e68b199a15d23eb15b5ff3dd8a20a68b1f3eadfa
cmeylan <159033943+Theeb7274@users.noreply.github.com>
2025-05-19 12:21:39 +0100

    Update Admin Check.ps1

--- Powershell/Admin Check.ps1
+++ Powershell/Admin Check.ps1
@@ -1,4 +1,8 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Error "This script requires Administrator privileges. Please re-run PowerShell as Administrator."
-    exit 1
+# A function to check the script has been ran with administrator rights, and exits if not
+function AdminCheck {
+
+    if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Error "This script requires Administrator privileges. Please re-run PowerShell as Administrator."
+        exit 1
+    }
 }
