a60f74c2ac754d0108ae0a42774efd4c8180f967
cmeylan <159033943+Theeb7274@users.noreply.github.com>
2025-05-19 14:48:31 +0100

    Update Service restart if stopped.ps1
    Added some notes

--- Powershell/Service restart if stopped.ps1
+++ Powershell/Service restart if stopped.ps1
@@ -1,10 +1,16 @@
-﻿function ServiceCheck {
+# Starts $serviceName if it's stopped
+# This script isn't a reliable way of keeping a service alive, this was made to catch a remote access service which has been flakey
+# This will be revisited if a more robust one is required
 
-    $serviceName = "Datto RMM"
+# Function to check the status of $serviceName and start if stopped 
+ function ServiceCheck {
+
+    $serviceName = ""
     $status = (Get-Service -Name $serviceName).status
         if ($status -ne "Running") {
         Start-Service -Name $serviceName
     }
 }
 
+# Call function
 ServiceCheck
