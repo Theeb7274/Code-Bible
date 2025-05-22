8483b7f5fb447fe6a5bf28fbe4ec1654a6e6146d
cmeylan <159033943+Theeb7274@users.noreply.github.com>
2025-05-22 08:31:57 +0100

    Update Call another ps1.ps1

--- Powershell/Call another ps1.ps1
+++ Powershell/Call another ps1.ps1
@@ -1,6 +1,6 @@
 # Calls another ps1 and passes paramaters onto it
 # If the script allows it, this can be used to pass general parameters aswell, such as -verbose
-# & tells powershell the following text is a script to be executed, otherwise it doesn't handle it correctly
+# & is a call operator which makes sure powershell handles $script as a command to be executed
 
 # Configuration
 $script = ""
