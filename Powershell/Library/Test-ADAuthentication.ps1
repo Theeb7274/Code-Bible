5b4f820e45b9b5a88b47ef1fbfb95de0bba6d42d
Theeb7274 <cammeylan@gmail.com>
2026-07-28 13:40:06 +0100

    Test commit from desktop

---
+++ Powershell/Library/Test-ADAuthentication.ps1
@@ -0,0 +1,7 @@
+Function Test-ADAuthentication {
+    param(
+        $username,
+        $password)
+    
+    (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
+}
